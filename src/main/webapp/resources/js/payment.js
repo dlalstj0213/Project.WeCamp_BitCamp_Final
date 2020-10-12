4583367d77c6d57094b94e5a60f9871d59a3d316
cateto <u2skind@gmail.com>
2020-10-12 18:23:17 +0900

    2020-10-12 : admin 사이드 메뉴 경로 수정

--- src/main/webapp/resources/js/payment.js
+++ src/main/webapp/resources/js/payment.js
@@ -96,7 +96,7 @@ function requestPay(){
 			msg += '상점 거래ID : ' + rsp.merchant_uid;
 			msg += '결제 금액 : ' + rsp.paid_amount;
 			msg += '카드 승인번호 : ' + rsp.apply_num;
-			alert(msg);
+			//alert(msg);
 		} else {
 			var errMsg = '결제에 실패하였습니다.';
 			errMsg += '에러내용 : ' + rsp.error_msg;
